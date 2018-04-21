ec95db987d1a273a2104966b7c303444885702d0
moujirmustapha <moujirmustapha@gmail.com>
2018-04-21 17:11:47 +0100

    New Update 17:11 21/04/2018

--- js/Script.js
+++ js/Script.js
@@ -15,6 +15,13 @@ $(function() {
     });
 });
 
+$(function() {
+    $('.Syl').click (function() {
+        $('html, body').animate({scrollTop: $('#about').offset().top }, 'slow');
+        return false;
+    });
+});
+
 
 $('.owl-carousel').owlCarousel({
     loop:true,
