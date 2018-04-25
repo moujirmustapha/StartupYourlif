9f49c411c893fd4d54a9b6e93aa173791e90a0d1
moujirmustapha <moujirmustapha@gmail.com>
2018-04-25 16:35:20 +0100

    New Update 16:34 25/04/2018

--- js/Script.js
+++ js/Script.js
@@ -47,6 +47,8 @@ $('.owl-carousel').owlCarousel({
 
 
 });
+
+// init Isotope
 var $grid = $('.grid').isotope({
     itemSelector: '.element-item',
     layoutMode: 'fitRows',
@@ -57,4 +59,33 @@ var $grid = $('.grid').isotope({
             return parseFloat( weight.replace( /[\(\)]/g, '') );
         }
     }
+});
+// bind filter button click
+$('#filters').on( 'click', 'button', function() {
+    var filterValue = $( this ).attr('data-filter');
+    // use filterFn if matches value
+    filterValue = filterFns[ filterValue ] || filterValue;
+    $grid.isotope({ filter: filterValue });
+});
+// filter functions
+var filterFns = {
+    // show if number is greater than 50
+    numberGreaterThan50: function() {
+        var number = $(this).find('.number').text();
+        return parseInt( number, 10 ) > 50;
+    },
+    // show if name ends with -ium
+    ium: function() {
+        var name = $(this).find('.name').text();
+        return name.match( /ium$/ );
+    }
+};
+
+// change is-checked class on buttons
+$('.button-group').each( function( i, buttonGroup ) {
+    var $buttonGroup = $(buttonGroup );
+    $buttonGroup.on( 'click', 'button', function() {
+        $buttonGroup.find('.is-checked').removeClass('is-checked');
+        $( this ).addClass('is-checked');
+    });
 });
